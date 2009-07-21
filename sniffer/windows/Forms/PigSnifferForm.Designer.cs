4b97f62214ce26d10f7f28140bb184d8be9fa172
arpmipg <arpmipg@a912ed4e-b18b-11dd-8714-116ca565c52f>
2009-07-21 14:13:19 +0000

    two split containers were added

--- sniffer/windows/Forms/PigSnifferForm.Designer.cs
+++ sniffer/windows/Forms/PigSnifferForm.Designer.cs
@@ -29,8 +29,10 @@
     private void InitializeComponent()
     {
       System.Windows.Forms.Label nicsLabel;
-      this.ipTextBox = new System.Windows.Forms.TextBox();
-      this.label1 = new System.Windows.Forms.Label();
+      System.Windows.Forms.Panel pigSnifferTopPanel;
+      System.Windows.Forms.SplitContainer bottomSplitContainer;
+      System.Windows.Forms.SplitContainer topSplitContainer;
+      this.nicsComboBox = new System.Windows.Forms.ComboBox();
       this.startButton = new System.Windows.Forms.Button();
       this.stopButton = new System.Windows.Forms.Button();
       this.packetsListView = new System.Windows.Forms.ListView();
@@ -42,38 +44,52 @@
       this.protocolColumn = new System.Windows.Forms.ColumnHeader();
       this.packetTreeView = new System.Windows.Forms.TreeView();
       this.packetRichTextBox = new System.Windows.Forms.RichTextBox();
-      this.nicsComboBox = new System.Windows.Forms.ComboBox();
       nicsLabel = new System.Windows.Forms.Label();
+      pigSnifferTopPanel = new System.Windows.Forms.Panel();
+      bottomSplitContainer = new System.Windows.Forms.SplitContainer();
+      topSplitContainer = new System.Windows.Forms.SplitContainer();
+      pigSnifferTopPanel.SuspendLayout();
+      bottomSplitContainer.Panel1.SuspendLayout();
+      bottomSplitContainer.Panel2.SuspendLayout();
+      bottomSplitContainer.SuspendLayout();
+      topSplitContainer.Panel1.SuspendLayout();
+      topSplitContainer.Panel2.SuspendLayout();
+      topSplitContainer.SuspendLayout();
       this.SuspendLayout();
       // 
       // nicsLabel
       // 
       nicsLabel.AutoSize = true;
-      nicsLabel.Location = new System.Drawing.Point(299, 37);
+      nicsLabel.Location = new System.Drawing.Point(12, 15);
       nicsLabel.Name = "nicsLabel";
       nicsLabel.Size = new System.Drawing.Size(100, 13);
       nicsLabel.TabIndex = 7;
       nicsLabel.Text = "Network Interfaces:";
       // 
-      // ipTextBox
+      // pigSnifferTopPanel
       // 
-      this.ipTextBox.Location = new System.Drawing.Point(85, 34);
-      this.ipTextBox.Name = "ipTextBox";
-      this.ipTextBox.Size = new System.Drawing.Size(100, 20);
-      this.ipTextBox.TabIndex = 0;
+      pigSnifferTopPanel.Anchor = System.Windows.Forms.AnchorStyles.Top;
+      pigSnifferTopPanel.Controls.Add(nicsLabel);
+      pigSnifferTopPanel.Controls.Add(this.nicsComboBox);
+      pigSnifferTopPanel.Controls.Add(this.startButton);
+      pigSnifferTopPanel.Controls.Add(this.stopButton);
+      pigSnifferTopPanel.Location = new System.Drawing.Point(62, 12);
+      pigSnifferTopPanel.Name = "pigSnifferTopPanel";
+      pigSnifferTopPanel.Size = new System.Drawing.Size(682, 43);
+      pigSnifferTopPanel.TabIndex = 10;
       // 
-      // label1
+      // nicsComboBox
       // 
-      this.label1.AutoSize = true;
-      this.label1.Location = new System.Drawing.Point(59, 37);
-      this.label1.Name = "label1";
-      this.label1.Size = new System.Drawing.Size(20, 13);
-      this.label1.TabIndex = 1;
-      this.label1.Text = "IP:";
+      this.nicsComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+      this.nicsComboBox.FormattingEnabled = true;
+      this.nicsComboBox.Location = new System.Drawing.Point(118, 11);
+      this.nicsComboBox.Name = "nicsComboBox";
+      this.nicsComboBox.Size = new System.Drawing.Size(341, 21);
+      this.nicsComboBox.TabIndex = 9;
       // 
       // startButton
       // 
-      this.startButton.Location = new System.Drawing.Point(125, 84);
+      this.startButton.Location = new System.Drawing.Point(500, 9);
       this.startButton.Name = "startButton";
       this.startButton.Size = new System.Drawing.Size(75, 23);
       this.startButton.TabIndex = 2;
@@ -83,13 +99,55 @@
       // 
       // stopButton
       // 
-      this.stopButton.Location = new System.Drawing.Point(280, 84);
+      this.stopButton.Location = new System.Drawing.Point(597, 9);
       this.stopButton.Name = "stopButton";
       this.stopButton.Size = new System.Drawing.Size(75, 23);
       this.stopButton.TabIndex = 3;
       this.stopButton.Text = "Stop";
       this.stopButton.UseVisualStyleBackColor = true;
       this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+      // 
+      // bottomSplitContainer
+      // 
+      bottomSplitContainer.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                  | System.Windows.Forms.AnchorStyles.Left)
+                  | System.Windows.Forms.AnchorStyles.Right)));
+      bottomSplitContainer.Location = new System.Drawing.Point(12, 61);
+      bottomSplitContainer.Name = "bottomSplitContainer";
+      bottomSplitContainer.Orientation = System.Windows.Forms.Orientation.Horizontal;
+      // 
+      // bottomSplitContainer.Panel1
+      // 
+      bottomSplitContainer.Panel1.Controls.Add(topSplitContainer);
+      bottomSplitContainer.Panel1MinSize = 110;
+      // 
+      // bottomSplitContainer.Panel2
+      // 
+      bottomSplitContainer.Panel2.Controls.Add(this.packetRichTextBox);
+      bottomSplitContainer.Panel2MinSize = 50;
+      bottomSplitContainer.Size = new System.Drawing.Size(783, 541);
+      bottomSplitContainer.SplitterDistance = 407;
+      bottomSplitContainer.TabIndex = 11;
+      // 
+      // topSplitContainer
+      // 
+      topSplitContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+      topSplitContainer.Location = new System.Drawing.Point(0, 0);
+      topSplitContainer.Name = "topSplitContainer";
+      topSplitContainer.Orientation = System.Windows.Forms.Orientation.Horizontal;
+      // 
+      // topSplitContainer.Panel1
+      // 
+      topSplitContainer.Panel1.Controls.Add(this.packetsListView);
+      topSplitContainer.Panel1MinSize = 50;
+      // 
+      // topSplitContainer.Panel2
+      // 
+      topSplitContainer.Panel2.Controls.Add(this.packetTreeView);
+      topSplitContainer.Panel2MinSize = 50;
+      topSplitContainer.Size = new System.Drawing.Size(783, 407);
+      topSplitContainer.SplitterDistance = 189;
+      topSplitContainer.TabIndex = 12;
       // 
       // packetsListView
       // 
@@ -100,14 +158,15 @@
             this.destIPColumn,
             this.destPortColumn,
             this.protocolColumn});
+      this.packetsListView.Dock = System.Windows.Forms.DockStyle.Fill;
       this.packetsListView.FullRowSelect = true;
       this.packetsListView.GridLines = true;
       this.packetsListView.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
       this.packetsListView.HideSelection = false;
-      this.packetsListView.Location = new System.Drawing.Point(27, 137);
+      this.packetsListView.Location = new System.Drawing.Point(0, 0);
       this.packetsListView.MultiSelect = false;
       this.packetsListView.Name = "packetsListView";
-      this.packetsListView.Size = new System.Drawing.Size(748, 178);
+      this.packetsListView.Size = new System.Drawing.Size(783, 189);
       this.packetsListView.TabIndex = 4;
       this.packetsListView.UseCompatibleStateImageBehavior = false;
       this.packetsListView.View = System.Windows.Forms.View.Details;
@@ -145,55 +204,46 @@
       // 
       // packetTreeView
       // 
-      this.packetTreeView.Location = new System.Drawing.Point(27, 335);
+      this.packetTreeView.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.packetTreeView.Location = new System.Drawing.Point(0, 0);
       this.packetTreeView.Name = "packetTreeView";
-      this.packetTreeView.Size = new System.Drawing.Size(748, 140);
+      this.packetTreeView.Size = new System.Drawing.Size(783, 214);
       this.packetTreeView.TabIndex = 5;
       // 
       // packetRichTextBox
       // 
-      this.packetRichTextBox.Location = new System.Drawing.Point(27, 493);
+      this.packetRichTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.packetRichTextBox.Location = new System.Drawing.Point(0, 0);
       this.packetRichTextBox.Name = "packetRichTextBox";
       this.packetRichTextBox.ReadOnly = true;
-      this.packetRichTextBox.Size = new System.Drawing.Size(748, 96);
+      this.packetRichTextBox.Size = new System.Drawing.Size(783, 130);
       this.packetRichTextBox.TabIndex = 6;
       this.packetRichTextBox.Text = "";
-      // 
-      // nicsComboBox
-      // 
-      this.nicsComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-      this.nicsComboBox.FormattingEnabled = true;
-      this.nicsComboBox.Location = new System.Drawing.Point(405, 33);
-      this.nicsComboBox.Name = "nicsComboBox";
-      this.nicsComboBox.Size = new System.Drawing.Size(285, 21);
-      this.nicsComboBox.TabIndex = 9;
       // 
       // PigSnifferForm
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(807, 611);
-      this.Controls.Add(this.nicsComboBox);
-      this.Controls.Add(nicsLabel);
-      this.Controls.Add(this.packetRichTextBox);
-      this.Controls.Add(this.packetTreeView);
-      this.Controls.Add(this.packetsListView);
-      this.Controls.Add(this.stopButton);
-      this.Controls.Add(this.startButton);
-      this.Controls.Add(this.label1);
-      this.Controls.Add(this.ipTextBox);
+      this.ClientSize = new System.Drawing.Size(807, 614);
+      this.Controls.Add(bottomSplitContainer);
+      this.Controls.Add(pigSnifferTopPanel);
       this.Name = "PigSnifferForm";
       this.Text = "PigSniffer";
       this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.pigSnifferForm_FormClosing);
+      pigSnifferTopPanel.ResumeLayout(false);
+      pigSnifferTopPanel.PerformLayout();
+      bottomSplitContainer.Panel1.ResumeLayout(false);
+      bottomSplitContainer.Panel2.ResumeLayout(false);
+      bottomSplitContainer.ResumeLayout(false);
+      topSplitContainer.Panel1.ResumeLayout(false);
+      topSplitContainer.Panel2.ResumeLayout(false);
+      topSplitContainer.ResumeLayout(false);
       this.ResumeLayout(false);
-      this.PerformLayout();
 
     }
 
     #endregion
 
-    private System.Windows.Forms.TextBox ipTextBox;
-    private System.Windows.Forms.Label label1;
     private System.Windows.Forms.Button startButton;
     private System.Windows.Forms.Button stopButton;
     private System.Windows.Forms.ListView packetsListView;
