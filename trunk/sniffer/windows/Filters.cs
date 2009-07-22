d55e32897971c8e2745c330831680a188179c5f3
arpmipg <arpmipg@a912ed4e-b18b-11dd-8714-116ca565c52f>
2009-07-22 16:04:43 +0000

    Filters form was added

---
+++ trunk/sniffer/windows/Filters.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+
+namespace PigSniffer
+{
+  /// <summary>
+  /// Represents values' pair.
+  /// </summary>
+  /// <typeparam name="T"></typeparam>
+  class Range<T>
+  {
+    public T First { get; private set; }
+    public T Second { get; private set; }
+
+
+    public Range(T first, T second)
+    {
+      First = first;
+      Second = second;
+    }
+
+  }
+
+
+  /// <summary>
+  /// Packets' filters: source port, source IP, destination port, destination IP.
+  /// </summary>
+  public class Filters
+  {
+    private readonly List<Range<int>> srcPorts = new List<Range<int>>();
+
+
+    /// <summary>
+    /// Parses input string and updates source port filters.
+    /// </summary>
+    /// <param name="srcPortsString">sourse ports as string</param>
+    /// <returns>true if srcPortsString is valid, false otherwise</returns>
+    public bool SetSrcPorts(string srcPortsString)
+    {
+      if (string.IsNullOrEmpty(srcPortsString))
+      {
+        srcPorts.Clear();
+        return true;
+      }
+
+      string[] srsPortsSubstrings = srcPortsString.Split(new[] {','});
+
+      return true;
+    }
+
+  }
+
+}
---
+++ trunk/sniffer/windows/Forms/FiltersForm.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows.Forms;
+
+
+namespace PigSniffer.Forms
+{
+  public partial class FiltersForm : Form
+  {
+    /// <summary>
+    /// packets filters from PigSnifferForm object
+    /// </summary>
+    private readonly Filters filters;
+
+
+    public FiltersForm(Filters filters)
+    {
+      this.filters = filters;
+
+      InitializeComponent();
+    }
+
+
+    #region Controls events
+
+    private void OKButton_Click(object sender, EventArgs e)
+    {
+      // TODO: save
+      Close();
+    }
+
+
+    private void CancelButton_Click(object sender, EventArgs e)
+    {
+      Close();
+    }
+
+    #endregion
+
+
+    public Filters GetFilters()
+    {
+      return filters;
+    }
+
+  }
+
+}
